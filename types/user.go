7ff338bdca26165414a806d4b90b490aff119c5f
Yoel Benitez <ybencab@gmail.com>
2025-01-27 18:17:49 +0000

    omiting user password in json

--- types/user.go
+++ types/user.go
@@ -18,7 +18,7 @@ type User struct {
 	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
