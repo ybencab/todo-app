0880a66825d23eaed094a9c0df89cae1988850d8
Yoel Benitez <ybencab@gmail.com>
2025-01-29 18:34:58 +0000

    todo creation handler

--- handlers/todo.go
+++ handlers/todo.go
@@ -4,8 +4,12 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/google/uuid"
 	"github.com/ybencab/todo-app/store"
+	"github.com/ybencab/todo-app/types"
 	"github.com/ybencab/todo-app/utils"
+	"github.com/ybencab/todo-app/validators"
+	"github.com/ybencab/todo-app/views/components"
 	"github.com/ybencab/todo-app/views/todo"
 )
 
@@ -29,7 +33,48 @@ func (h *ToDoHandler) HandleTodo(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *ToDoHandler) HandleCreateTodo(w http.ResponseWriter, r *http.Request) {
+	// Parse form data
+	if err := r.ParseForm(); err != nil {
+		components.ToDoForm("", "", "Invalid form data", "").Render(r.Context(), w)
+		return
+	}
 
+	// Getting form values and validate them
+	title := r.FormValue("title")
+	description := r.FormValue("description")
+	if err := validators.ValidateCreateTodoRequest(title, description); err != nil {
+		components.ToDoForm(title, description, err.Error(), "").Render(r.Context(), w)
+		return
+	}
+
+	// Get user data
+	userData, ok := utils.GetUserDataFromContext(r.Context())
+	if !ok {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// Check if todo already exists
+	if _, err := h.store.GetTodo(userData.ID, title); err == nil {
+		components.ToDoForm(title, description, "Todo already exists", "").Render(r.Context(), w)
+		return
+	}
+
+	// Convert user_id to uuid
+	user_id, err := uuid.Parse(userData.ID)
+	if err != nil {
+		components.ToDoForm(title, description, "Error creating todo", "").Render(r.Context(), w)
+		return
+	}
+
+	// Create todo
+	new_todo := types.NewTodo(user_id, title, description)
+	if err := h.store.CreateTodo(new_todo); err != nil {
+		components.ToDoForm(title, description, "Error creating todo", "").Render(r.Context(), w)
+		return
+	}
+	
+	components.ToDoForm("", "", "", "Todo created").Render(r.Context(), w)
 }
 
 func (h *ToDoHandler) HandletGetTodos(w http.ResponseWriter, r *http.Request) {
